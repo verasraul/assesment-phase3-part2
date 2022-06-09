639fc94ffab8d93e4cf80e059bd5446ff7e324b4
Raul Veras <verasraul@gmail.com>
2022-06-08 20:29:21 -0400

    adding dotenv requirement for db connection

--- db/index.js
+++ db/index.js
@@ -1,15 +1,14 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
+require("dotenv").config()
 
+MONGO_URI = process.env.MONGO_URI;
 
-let MONGO_URI = process.env.MONGO_URI;
-
-mongoose
-  .connect(MONGO_URI, {
-    useUnifiedTopology: true,
+mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
   .then(() => console.log("Succesfully Connected to Database"))
   .catch((err) => console.error("Connection Error", err.message));
 
-let db = mongoose.connection;
-module.exports = db;
+const db = mongoose.connection
+module.exports = db
