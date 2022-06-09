8cbf6dee9dee3fda064593f06b85dfb9e9feecca
Raul Veras <verasraul@gmail.com>
2022-06-08 20:05:54 -0400

    creating controllers logic

---
+++ models/lambRecipes.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const LambRecipes = new Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+  },
+
+  origin_country: {
+    type: String,
+    required: false,
+    trim: true,
+    lowercase: true,
+  },
+
+  country_flag: {
+    type: String,
+    required: false,
+    trim: true,
+    lowercase: true,
+  },
+
+  ingredients: {
+    type: String,
+    required: false,
+    trim: true,
+    lowercase: true,
+  },
+
+  picture: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+  },
+});
+
+const Recipe = mongoose.model("lambRecipes", LambRecipeSchema);
+
+module.exports = Recipe;
